e4b519186e0234e405512bf50f212943bc166464
Prajwal W <prajwalwaingankar@gmail.com>
2021-03-19 18:04:55 +0530

    Added my demo created credentials.

---
+++ app/src/main/java/com/example/tiledesk/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.tiledesk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBtn(View v)
+    {
+        startActivity(new Intent(this, TiledeskActivity.class));
+    }
+
+}
